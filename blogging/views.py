f4f9e35ae1a1442dc433a9df905fc3f959cf2177
edina do nascimento silva <edinas09@gmail.com>
2021-11-30 14:01:02 -0300

    add features assignment 8

---
+++ blogging/views.py
@@ -0,0 +1,64 @@
+# blogging/views.py
+
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from blogging.models import Post
+
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+
+def stub_view(request, *args, **kwargs):
+    body = "Stub View\n\n"
+    if args:
+        body += "Args:\n"
+        body += "\n".join(["\t%s" % a for a in args])
+    if kwargs:
+        body += "Kwargs:\n"
+        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+    return HttpResponse(body, content_type="text/plain")
+
+
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     context = {'posts': posts}
+#     return render(request, 'list.html', context)
+
+class BlogListView(ListView):
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'blogging/list.html'
+    # model = Post
+    # template_name = 'blogging/list.html'
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get a context
+    #     context = super().get_context_data(**kwargs)
+    #     published = Post.objects.exclude(published_date__exact=None)
+    #     # Add in the publisher
+    #     context['posts'] = published.order_by('-published_date')
+    #     return context
+
+class BlogDetailView(DetailView):
+    queryset = Post.objects.exclude(published_date__exact=None)
+    template_name = 'blogging/detail.html'
+    # model = Post
+    # template_name = 'blogging/detail.html'
+    #
+    # def get(self, request, *args, **kwargs):
+    #     post = self.get_object()
+    #     context = {'object': post}
+    #     return render(request, "blogging/detail.html", context)
+
+
+
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
+
+
---
+++ polling/urls.py
@@ -0,0 +1,9 @@
+# polling/urls.py
+
+from django.urls import path
+from polling.views import PoolListView, PoolDetailView
+
+urlpatterns = [
+    path('', PoolListView.as_view(), name="poll_index"),
+    path('polls/<int:pk>/', PoolDetailView.as_view(), name="poll_detail"),
+]
